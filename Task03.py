13bb31051f9901ef927cea19e28e3146e07a30d7
Sviatoslav <ecrprague.asi@gmail.com>
2023-02-10 01:25:36 +0300

    T3D

--- Task03.py
+++ Task03.py
@@ -6,3 +6,25 @@
 # Необходимо написать программу, открывающую файл на чтение и считывающую построчно данные.
 # При этом английские числительные должны заменяться на русские.
 # Новый блок строк должен записываться в новый текстовый файл.
+
+my_dic =\
+    {
+        'One': 'Один',
+        'Two': 'Два',
+        'Three': 'Три',
+        'Four': 'Четыре'
+    }
+
+stream_to_w = []
+
+with open('Task03.txt', 'r', encoding='utf-8') as stream_r:
+
+    for i in stream_r:
+        i = i.split(' ', 1)
+        stream_to_w.append(my_dic[i[0]] + ' ' + i[1])
+
+
+print(stream_to_w)
+
+with open('Task03_w.txt', 'w', encoding='utf-8') as stream_w:
+    stream_w.writelines(stream_to_w)
